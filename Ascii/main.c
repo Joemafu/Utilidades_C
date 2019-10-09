45a1193f8b279ff2f1bc560adf2329f6c464e3e3
joemafu <joe_mafu@hotmail.com>
2019-10-09 12:28:20 -0300

    Inicializando repositorio

---
+++ Ascii/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    int ch;
+
+    for(i=0;i<1000;i++)
+    {
+        ch=getche();
+        printf("\ %d\n",ch);
+    }
+
+
+    return 0;
+}
---
+++ Biblioteca de funciones/main.c
@@ -0,0 +1,30 @@
+#include "ArrayEmployees.h"
+
+int main()
+{
+    int option;
+    option=showMenuGetOption("Ingrese opcion.\n",1,9);
+
+    switch (option)
+    {
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        break;
+    case 6:
+        break;
+    case 7:
+        break;
+    case 8:
+        break;
+    case 9:
+        break;
+    }
+    return 0;
+}
