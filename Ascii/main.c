9d99a22287cf2f16d899eab859cf2cad49d2829d
Joel Mahafud <joe_mafu@hotmail.com>
2022-09-16 16:20:51 -0300

    Actualizando

--- Ascii/main.c
+++ Ascii/main.c
@@ -4,14 +4,21 @@
 int main()
 {
     int i;
-    int ch;
 
-    for(i=0;i<1000;i++)
+    printf("Ingrese un caracter para ver su valor ASCII o \"?\" para ver todos los caracteres:\n");
+    while(i==i)
     {
-        ch=getche();
-        printf("\ %d\n",ch);
+        i=getche();
+        printf(" %d\n",i);
+
+        if(i=='?')
+        {
+            for(i=1;i<=255;i++)
+            {
+                printf("\n%d - %c",i,i);
+            }
+            printf("\n");
+        }
     }
-
-
     return 0;
 }
